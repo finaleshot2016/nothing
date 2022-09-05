0d0ba32d33924027f70f82d0ea7bc3e27227c125
FINALESHOT2016 <103410982+finaleshot2016@users.noreply.github.com>
2022-09-06 02:52:05 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,8 +14,8 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-            <a href="https://twitter.com/TheUnownedERC20" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
+            <a href="https://twitter.com/TheUnownedERC20" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="/" /></a>
+                <a href="/"><img src={navIcon2} alt="/" /></a>
                 <a href="https://medium.com/@TheUnowned/unowned-e2080fb81d8e" target="_blank" rel="noopener noreferrer"><img src={navIcon3} alt="" /></a>
             </div>
             <p>Copyright 2022. UNOWNED</p>
